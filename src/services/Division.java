afec5d3c1584fe79ba86696f0e6fec9ba55b3aa4
Gabriel Santos <gabrielcarneirocsantos42@gmail.com>
2024-06-26 07:20:25 -0300

    Classe Division

---
+++ src/services/Division.java
@@ -0,0 +1,14 @@
+package services;
+
+import Exceptions.Demon0Exception;
+
+public class Division implements CalculateInterface{
+
+    @Override
+    public void calculate(double a, double b){
+        if(b <= 0){
+            throw new Demon0Exception("Nao pode dividir por zero");
+        }
+        System.out.println(a / b);
+    }
+}
